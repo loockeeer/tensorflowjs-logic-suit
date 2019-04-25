874a4528ebf6604b69425e270cd3902def73c290
loockeeer <34001773+loockeeer@users.noreply.github.com>
2019-04-25 19:04:31 +0200

    Bootstrap

--- script.js
+++ script.js
@@ -29,23 +29,26 @@ function setup() {
 
 function startTrain() {
     document.getElementById("train").innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Training...'
+    let logicInputs = [
+        document.getElementById("logicInput1").value.split(""),
+        document.getElementById("logicInput2").value.split(""),
+        document.getElementById("logicInput3").value.split(""),
+        document.getElementById("logicInput4").value.split("")
+    ]
     const xs = tf.tensor2d([
-    	[1, 0, 0], // 1 0 0 | 1 1 0 | 1 1 1 | 0 1 1 | 0 0 1 | 0 0 0 | 1 0 1 | 0 1 0
-    	[1, 1, 0],
-    	[1, 1, 1],
-    	[0, 1, 1],
-    	[0, 0, 1]
+    	[parseInt(logicInputs[0][0]), parseInt(logicInputs[0][1]), parseInt(logicInputs[0][2])],
+    	[parseInt(logicInputs[1][0]), parseInt(logicInputs[1][1]), parseInt(logicInputs[1][2])],
+    	[parseInt(logicInputs[2][0]), parseInt(logicInputs[2][1]), parseInt(logicInputs[2][2])],
+    	[parseInt(logicInputs[3][0]), parseInt(logicInputs[3][1]), parseInt(logicInputs[3][2])]
     	])
     const ys = tf.tensor1d([
-    	0,
-    	1,
-    	1,
-    	1,
-    	0
+    	parseInt(document.getElementById("resultInput1").value),
+        parseInt(document.getElementById("resultInput2").value),
+        parseInt(document.getElementById("resultInput3").value),
+        parseInt(document.getElementById("resultInput4").value)
     	]) 
     document.getElementById("train").innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Training...'
     model.fit(xs, ys, {epochs: document.getElementById("epochsRange").value}).then(()=>{
-    	console.log("Trained !")
         document.getElementById("train").innerHTML = 'Start train !'
         document.getElementById("predict").disabled = false
     })
@@ -53,12 +56,15 @@ function startTrain() {
 }
 
 function startPredict() {
+    let resultInputs = [
+        parseInt(document.getElementById("logicInputForResult").value.split("")[0]),
+        parseInt(document.getElementById("logicInputForResult").value.split("")[1]),
+        parseInt(document.getElementById("logicInputForResult").value.split("")[2])
+    ]
 	const data = tf.tensor2d([
-		[0, 0, 0]
+		[resultInputs[0], resultInputs[1], resultInputs[2]]
 	])
 	let result = model.predict(data)
-	console.log("Result : "+result.toString())
-	document.getElementById("result").innerHTML = "Result : "+result.toString()
     document.getElementById("result-modal-body").innerHTML = "The neural network predicted : "+result.toString()
     $('#exampleModal').modal('show')
 }
