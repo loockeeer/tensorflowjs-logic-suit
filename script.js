3a7b75ecfa77ae419f3010dfe61f7f6fa2c55a51
loockeeer <34001773+loockeeer@users.noreply.github.com>
2019-04-27 16:32:30 +0200

    Add advanced settings

--- script.js
+++ script.js
@@ -6,51 +6,59 @@ let setuped = false
 function setup() {
     setuped = true
     document.getElementById("train").disabled = false
-	model = tf.sequential();
-	const hiddenLayer = tf.layers.dense({
-    units: parseInt(document.getElementById("neuronsRange").value),
-    inputShape: [parseInt(document.getElementById("logicInputLength").value)],
-    activation: "sigmoid"
-	})
+    model = tf.sequential();
+    const hiddenLayer = tf.layers.dense({
+        units: parseInt(document.getElementById("neuronsRange").value),
+        inputShape: [parseInt(document.getElementById("logicInputLength").value)],
+        activation: document.getElementById("activationFunctionSelector")[document.getElementById("activationFunctionSelector").selectedIndex].innerHTML.toLowerCase()
+    })
+    const outputLayer = tf.layers.dense({
+        units: 1,
+        activation: document.getElementById("activationFunctionSelector")[document.getElementById("activationFunctionSelector").selectedIndex].innerHTML.toLowerCase()
+    })
+    model.add(hiddenLayer);
+    model.add(outputLayer);
 
-	const outputLayer = tf.layers.dense({
-	    units: 1,
-	    activation: "sigmoid"
-	})
-
-	model.add(hiddenLayer);
-	model.add(outputLayer);
-
-	model.compile({
-	    optimizer: "rmsprop",
-	    loss: tf.losses.meanSquaredError,
+    model.compile({
+        optimizer: document.getElementById("optimizerSelector")[document.getElementById("optimizerSelector").selectedIndex].innerHTML,
+        loss: tf.losses[document.getElementById("lossFunctionSelector")[document.getElementById("lossFunctionSelector").selectedIndex].innerHTML],
         metrics: ['mae']
 
-	})
+    })
 }
 
 function startTrain() {
     document.getElementById("train").innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Training...'
     let logicInputs = [
-        document.getElementById("logicInput1").value.split("").map(x => {return parseInt(x)}),
-        document.getElementById("logicInput2").value.split("").map(x => {return parseInt(x)}),
-        document.getElementById("logicInput3").value.split("").map(x => {return parseInt(x)}),
-        document.getElementById("logicInput4").value.split("").map(x => {return parseInt(x)})
+        document.getElementById("logicInput1").value.split("").map(x => {
+            return parseInt(x)
+        }),
+        document.getElementById("logicInput2").value.split("").map(x => {
+            return parseInt(x)
+        }),
+        document.getElementById("logicInput3").value.split("").map(x => {
+            return parseInt(x)
+        }),
+        document.getElementById("logicInput4").value.split("").map(x => {
+            return parseInt(x)
+        })
     ]
     const xs = tf.tensor2d([
-    	logicInputs[0],
-    	logicInputs[1],
-    	logicInputs[2],
-    	logicInputs[3]
-    	])
+        logicInputs[0],
+        logicInputs[1],
+        logicInputs[2],
+        logicInputs[3]
+    ])
     const ys = tf.tensor1d([
-    	parseInt(document.getElementById("resultInput1").value),
+        parseInt(document.getElementById("resultInput1").value),
         parseInt(document.getElementById("resultInput2").value),
         parseInt(document.getElementById("resultInput3").value),
         parseInt(document.getElementById("resultInput4").value)
-    	]) 
+    ])
     document.getElementById("train").innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Training...'
-    model.fit(xs, ys, {epochs: document.getElementById("epochsRange").value}).then(()=>{
+    model.fit(xs, ys, {
+        epochs: document.getElementById("epochsRange").value
+    }).then(() => {
         document.getElementById("train").innerHTML = 'Start train !'
         document.getElementById("predict").disabled = false
     })
@@ -58,11 +66,13 @@ function startTrain() {
 }
 
 function startPredict() {
-    let logicInput = document.getElementById("logicInputForResult").value.split("").map(x => {return parseInt(x)})
-	const data = tf.tensor2d([
-		logicInput
-	])
-	let result = model.predict(data)
-    document.getElementById("result-modal-body").innerHTML = "The neural network predicted : "+result.toString()
+    let logicInput = document.getElementById("logicInputForResult").value.split("").map(x => {
+        return parseInt(x)
+    })
+    const data = tf.tensor2d([
+        logicInput
+    ])
+    let result = model.predict(data)
+    document.getElementById("result-modal-body").innerHTML = "The neural network predicted : " + result.toString()
     $('#exampleModal').modal('show')
 }
